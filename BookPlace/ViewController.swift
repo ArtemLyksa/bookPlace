841c156692cd2d21a32aa26e014a349a98d104a8
Artem Lyksa <artemlyksa@Artems-MacBook-Pro.local>
2017-03-08 14:26:09 +0200

    Initial Commit

---
+++ BookPlace/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BookPlace
+//
+//  Created by Artem Lyksa on 3/8/17.
+//  Copyright © 2017 Artem Lyksa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
