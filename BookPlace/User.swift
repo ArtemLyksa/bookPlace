b235f887eb27748bb6818e5938032d35950b860b
Artem Lyksa <artemlyksa@Artems-MacBook-Pro.local>
2017-03-08 17:32:43 +0200

    Facebook and Google auth

---
+++ BookPlace/User.swift
@@ -0,0 +1,19 @@
+//
+//  User.swift
+//  BookPlace
+//
+//  Created by Artem Lyksa on 3/8/17.
+//  Copyright © 2017 Artem Lyksa. All rights reserved.
+//
+
+import Foundation
+
+class User: NSObject {
+    var name: String?
+    var email: String?
+    
+    required init(name: String, email: String) {
+        self.name = name
+        self.email = email
+    }
+}
